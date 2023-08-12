6a0c5490d5073c49b57f06cd9cccc36a4660ff7d
faishal hanif <budijansen88@gmail.com>
2023-08-12 17:46:51 +0700

    fixed navigation site

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const odinOption = document.querySelector(".odin");
 
 
 breadlyOption.addEventListener("click", (e) => {
-    window.location.href = "/forms/breadly/breadly.html";
+    window.location.href = "forms/breadly/breadly.html";
 });
 
 odinOption.addEventListener("click", (e) => {
-    window.location.href = "/forms/odin/odin.html";
+    window.location.href = "forms/odin/odin.html";
 });
 
 
